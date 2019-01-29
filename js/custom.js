6422f9b341e2f5cdc97162c73ca876164dc90048
Frank Fang <FrankFang1990@gmail.com>
2019-01-29 14:17:25 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
 {
+  const view = {
+    button: undefined,
+    init() {
+      this.button = document.querySelector('#upvote')
+    }
+  }
   const model = {
     _count: 1,
     get count() {return this._count},
@@ -10,45 +16,52 @@
       this.count += value
     }
   }
-  const controller = () => {
-    const button = document.querySelector('#upvote')
-    const query = new AV.Query('Upvote');
-    query.limit(1000)
-    query.find().then((upvotes) => {
-      model.count = upvotes.length
-    }, (e) => {
-      console.log(e)
-    })
-    if (button) {
-      const done = () => button.disabled = false
-      const resolved = () => {
-        model.addCount(1)
-        done()
+
+  const controller = {
+    init() {
+      view.init()
+      this.hideButtonIfClicked()
+      this.getCount()
+      this.bindEvents()
+    },
+    hideButtonIfClicked() {
+      if (localStorage.getItem('voted')) {
+        view.button.remove()
       }
-      const rejected = (e) => {
+    },
+    getCount() {
+      const query = new AV.Query('Upvote');
+      query.limit(1000)
+      query.find().then((upvotes) => {
+        model.count = upvotes.length
+      }, (e) => {
         console.log(e)
-        done()
-      }
-      button.onclick = () => {
-        button.disabled = true
-        const Upvote = AV.Object.extend('Upvote');
-        const upvote = new Upvote();
-        upvote.save({
-          target: window.location.href
-        }).then(resolved, rejected)
+      })
+    },
+    bindEvents() {
+      const button = view.button;
+      if (button) {
+        const done = () => button.disabled = false
+        const resolved = () => {
+          model.addCount(1)
+          localStorage.setItem('voted', 'yes')
+          button.remove()
+          done()
+        }
+        const rejected = (e) => {
+          console.log(e)
+          done()
+        }
+        button.onclick = () => {
+          button.disabled = true
+          const Upvote = AV.Object.extend('Upvote');
+          const upvote = new Upvote();
+          upvote.save({
+            target: window.location.href
+          }).then(resolved, rejected)
+        }
       }
     }
-
-    var config = {
-      apiKey: "AIzaSyB30FyEhU0NdQsn6WiVaZiOadfDGbHNq7U",
-      authDomain: "godie996-74232.firebaseapp.com",
-      databaseURL: "https://godie996-74232.firebaseio.com",
-      projectId: "godie996-74232",
-      storageBucket: "godie996-74232.appspot.com",
-      messagingSenderId: "54493442820"
-    };
-    firebase.initializeApp(config);
-
   }
-  window.addEventListener('load', controller)
+  window.addEventListener('load', () => controller.init())
 }
